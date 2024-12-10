af6c729029ab1b5097175b54be49ca4be37224d4
Kaz149 <quangtung149@gmail.com>
2024-12-10 22:07:34 +0700

    fix

--- lib/email_list.dart
+++ lib/email_list.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'email_model.dart';
 import 'email_detail.dart';
 
@@ -27,7 +25,7 @@ class _EmailListState extends State<EmailList> {
       isStarred: false,
       labels: [],
       isTrashed: false,
-      isDraft: false, 
+      isDraft: false,
     ),
     Email(
       id: '2',
@@ -40,7 +38,7 @@ class _EmailListState extends State<EmailList> {
       isStarred: false,
       labels: [],
       isTrashed: false,
-      isDraft: false, 
+      isDraft: false,
     ),
     Email(
       id: '3',
@@ -53,7 +51,7 @@ class _EmailListState extends State<EmailList> {
       isStarred: false,
       labels: [],
       isTrashed: false,
-      isDraft: false, 
+      isDraft: false,
     ),
     Email(
       id: '4',
@@ -66,9 +64,9 @@ class _EmailListState extends State<EmailList> {
       isStarred: false,
       labels: [],
       isTrashed: false,
-      isDraft: false, 
+      isDraft: false,
     ),
-     Email(
+    Email(
       id: '5',
       from: 'parents@gmail.com',
       to: 'you@gmail.com',
@@ -79,9 +77,9 @@ class _EmailListState extends State<EmailList> {
       isStarred: false,
       labels: [],
       isTrashed: false,
-      isDraft: false, 
+      isDraft: false,
     ),
-     Email(
+    Email(
       id: '6',
       from: 'anh@gmail.com',
       to: 'you@gmail.com',
@@ -92,89 +90,47 @@ class _EmailListState extends State<EmailList> {
       isStarred: false,
       labels: [],
       isTrashed: false,
-      isDraft: false, 
+      isDraft: false,
     ),
   ];
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    Query query = FirebaseFirestore.instance.collection('emails');
-    switch (widget.category) {
-      case 'inbox':
-        query = query.where('to', isEqualTo: FirebaseAuth.instance.currentUser!.email).where('isTrashed', isEqualTo: false);
-        break;
-      case 'sent':
-        query = query.where('from', isEqualTo: FirebaseAuth.instance.currentUser!.email);
-        break;
-      case 'drafts':
-        query = query.where('isDraft', isEqualTo: true);
-        break;
-      case 'starred':
-        query = query.where('isStarred', isEqualTo: true);
-        break;
-      case 'trashed':
-        query = query.where('isTrashed', isEqualTo: true);
-        break;
+    List<Email> emails = [];
+
+    if (widget.category == 'inbox') {
+      emails = mockEmails;
     }
 
-    if (widget.keyword != null && widget.keyword!.isNotEmpty) {
-      query = query.where('subject', isGreaterThanOrEqualTo: widget.keyword).where('subject', isLessThanOrEqualTo: widget.keyword! + '\uf8ff');
-    }
-
-    return StreamBuilder(
-      stream: query.snapshots(),
-      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        List<Email> emails = snapshot.data!.docs.map((doc) => Email(
-          id: doc.id,
-          from: doc['from'],
-          to: doc['to'],
-          subject: doc['subject'],
-          body: doc['body'],
-          date: doc['date'].toDate(),
-          isRead: doc['isRead'],
-          isStarred: doc['isStarred'],
-          labels: List<String>.from(doc['labels']),
-          isTrashed: doc['isTrashed'],
-          isDraft: doc['isDraft'],
-        )).toList();
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Emails - ${widget.category.capitalize()}'),
         
-        if (widget.category != 'trashed' && widget.category != 'drafts') {
-          if (widget.category == 'starred') {
-            emails.addAll(mockEmails.where((email) => email.isStarred));
-          } else {
-            emails.addAll(mockEmails);
-          }
-        }
-
-        return ListView.builder(
-          itemCount: emails.length,
-          itemBuilder: (context, index) {
-            var email = emails[index];
-            return Card(
-              child: ListTile(
-                title: Text(email.subject, style: TextStyle(fontWeight: email.isRead ? FontWeight.normal : FontWeight.bold)),
-                subtitle: Text('${email.from} • ${email.body}'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => EmailDetails(email: email)),
-                  );
-                },
-              ),
-            );
-          },
-        );
-      },
+      ),
+      body: ListView.builder(
+        itemCount: emails.length,
+        itemBuilder: (context, index) {
+          var email = emails[index];
+          return Card(
+            child: ListTile(
+              title: Text(email.subject, style: TextStyle(fontWeight: email.isRead ? FontWeight.normal : FontWeight.bold)),
+              subtitle: Text('${email.from} • ${email.body}'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EmailDetails(email: email)),
+                );
+              },
+            ),
+          );
+        },
+      ),
     );
+  }
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${substring(1)}";
   }
 }
